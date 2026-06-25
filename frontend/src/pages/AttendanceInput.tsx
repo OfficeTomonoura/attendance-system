5c55ce6be2a79d28cbaa41793e738103f50aadfd
Rui Tokunaga <tokunaga-rui@officetomonoura.jp>
2026-06-25 09:16:12 +0900

    fix: resolve parenthesis and div syntax error in AttendanceInput

--- frontend/src/pages/AttendanceInput.tsx
+++ frontend/src/pages/AttendanceInput.tsx
@@ -578,7 +578,7 @@ export default function AttendanceInput() {
             </DndContext>
           </div>
         </div>
-      )}
+      ))}
 
       {/* VALIDATION ERROR MODAL */}
       {showValidationModal && (
